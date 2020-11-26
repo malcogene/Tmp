7c1f5da3fde623391a92de11a5d35854ed1e5bb0
malgogenesis <palelamp@hotmail.com>
2020-11-26 19:24:58 +0900

    .

--- CDN/malcogene.js
+++ CDN/malcogene.js
@@ -1,22 +1,4 @@
-/* 처음 접속시, 입력 필드에 포커스 주기  */
-$(document).ready(function() {
-     $("a.thumb").click(function(event){
-     event.preventDefault();
-     var content = $(".modal-body");
-     content.empty();
-     var title = $(this).attr("title");
-     $(".modal-title").html(title);
-     content.html($(this).html());
-     $(".modal-profile").modal({show:true});
-     });
-     });
-$(document).on("shown.bs.modal", function(e) {
-  $("input:visible:enabled:first", e.target).focus();
-});
-$(document).ready( function() {
-  $("#txtEditor").Editor(); 
-});
-
+$(function()\{ $("h3").html("jquery test"); \});     \
 
 
 
